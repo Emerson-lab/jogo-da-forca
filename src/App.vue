b81390a106d116ec1f41ffea7c324c222702f8b4
Emerson-lab <trinddade29@gmail.com>
2022-02-08 10:39:29 -0300

    add form

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
   <div id="app">
     <h1>Jogo da Forca</h1>
+    <section id="inicio" v-if="tela === 'inicio'">
+      <Formulario />
+    </section>
+    <section id="jogo" v-if="tela === 'jogo'">Jogo</section>
   </div>
 </template>
 
 <script>
 import "./css/global.css";
 
+import Formulario from "./components/Formulario";
+
 export default {
   name: "App",
-  components: {},
+  data() {
+    return {
+      tela: "inicio",
+    };
+  },
+  components: {
+    Formulario,
+  },
 };
 </script>
 
