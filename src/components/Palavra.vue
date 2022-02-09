7ecd0c2cf42fcf3c1dd8af0f25f8be9888698b02
Emerson-lab <trinddade29@gmail.com>
2022-02-09 20:02:30 -0300

    create component Palavra

---
+++ src/components/Palavra.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="palavra">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Palavra",
+  props: {},
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {},
+};
+</script>
+
+<style scoped></style>
