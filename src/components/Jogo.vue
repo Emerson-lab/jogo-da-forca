458d2addd80fa6fd3b6007147dabb3c4eb029b0a
Emerson-lab <trinddade29@gmail.com>
2022-02-09 20:01:25 -0300

    create component Jogo

---
+++ src/components/Jogo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="jogo">
+    <Forca :erros="erros" />
+    <Palavra />
+  </div>
+</template>
+
+<script>
+import Forca from "./Forca";
+import Palavra from "./Palavra";
+
+export default {
+  name: "Jogo",
+  props: {
+    erros: Number,
+  },
+  data() {
+    return {};
+  },
+  components: { Forca, Palavra },
+  methods: {},
+};
+</script>
+
+<style scoped>
+.jogo {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
+</style>
