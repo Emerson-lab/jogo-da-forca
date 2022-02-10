f2a22a5dfbf63f4bd329906274aaf5416ee41bbe
Emerson-lab <trinddade29@gmail.com>
2022-02-10 12:11:14 -0300

    create jogo

--- src/components/Jogo.vue
+++ src/components/Jogo.vue
@@ -1,23 +1,55 @@
 <template>
   <div class="jogo">
     <Forca :erros="erros" />
-    <Palavra />
+    <Palavra
+      :palavra="palavra"
+      :dica="dica"
+      :verificarLetra="verificarLetra"
+      :etapa="etapa"
+    />
+    <Teclado
+      :letras="letras"
+      :verificarLetra="verificarLetra"
+      :jogar="jogar"
+      v-if="etapa === 'jogo'"
+    />
+
+    <Final
+      v-if="etapa !== 'jogo'"
+      :etapa="etapa"
+      :texto="etapa === 'enforcado' ? 'Perdeu :(' : 'Parabéns :)'"
+      :jogarNovamente="jogarNovamente"
+    />
   </div>
 </template>
 
 <script>
 import Forca from "./Forca";
 import Palavra from "./Palavra";
+import Teclado from "./Teclado";
+import Final from "./Final";
 
 export default {
   name: "Jogo",
   props: {
     erros: Number,
+    palavra: String,
+    dica: String,
+    verificarLetra: Function,
+    etapa: String,
+    letras: Array,
+    jogar: Function,
+    jogarNovamente: Function,
   },
   data() {
     return {};
   },
-  components: { Forca, Palavra },
+  components: {
+    Forca,
+    Palavra,
+    Teclado,
+    Final,
+  },
   methods: {},
 };
 </script>
